142e78849732d36f66966ad609757d99aa66d84f
Paul <paul.salina@gmail.com>
2016-09-19 20:50:28 -0400

    add dumby muscle reducer that just returns a default list

---
+++ js/reducers/muscles.js
@@ -0,0 +1,40 @@
+// @flow
+import type { Action } from './../actions/types'
+
+// default muscles used when not using remote
+const defaultMuscles: State = [
+  {
+    id : '1',
+    name : 'biceps'
+  },
+  {
+    id : '2',
+    name : 'chest'
+  },
+  {
+    id : '3',
+    name : 'back'
+  },
+  {
+    id : '4',
+    name : 'shoulders'
+  },
+  {
+    id : '5',
+    name : 'hamstrings'
+  }
+]
+
+type Muscle = {
+  id: string,
+  name: string
+}
+
+type State = Array<Muscle>
+
+export default function (state: State = defaultMuscles, action: Action) {
+  switch(action) {
+  default:
+    return state
+  }
+}
