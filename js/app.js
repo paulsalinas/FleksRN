801376ef1582159497dc4b1163ec2f9b6922b432
Paul <paul.salina@gmail.com>
2016-09-21 22:38:49 -0400

    add logging and uid packages

--- js/app.js
+++ js/app.js
@@ -6,14 +6,18 @@
 
 import React, { Component } from 'react'
 import FleksTabs from './components/FleksTabs'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import ui from './reducers/ui'
 import muscles from './reducers/muscles'
 import exercises from './reducers/exercises'
+import createLogger from 'redux-logger'
 
 let reducers = combineReducers({ muscles, exercises, ui })
-let store = createStore(reducers)
+let store = createStore(
+  reducers,
+  applyMiddleware(createLogger())
+)
 
 export default class FleksRN extends Component {
   render() {
